1c21d8841fa63b7b214f8d379ebf2c5ed0f78af4
Lucia Arrabali <carmenarrabali@gmail.com>
2021-03-16 17:28:13 +0100

    Añadido clase Fibonacci.java

--- src/main/java/org/pab2020/Fibonacci.java
+++ src/main/java/org/pab2020/Fibonacci.java
@@ -2,24 +2,22 @@ package org.pab2020;
 
 public class Fibonacci {
     /**
-     *
      * @param n = número de meses
+     * @param k = número de parejas de conejos
      * @return Fn = número de conejos después de un número de generaciones.
      */
-    public int fibonacci(int n){
-        int k = 0;
+    public int fibonacci(int n, int k){
         if (n < 0){
             throw new RuntimeException("El mes no puede ser negativo.");
         }
         else if(n==0){
             return 0;
         }
-
         else if(n==1 || n==2){
             return 1;
         }
         else{
-            return (fibonacci(n-1) + k * fibonacci(n-2));
+            return (fibonacci(n-1,k) + k * fibonacci(n-2, k));
         }
     }
 }
