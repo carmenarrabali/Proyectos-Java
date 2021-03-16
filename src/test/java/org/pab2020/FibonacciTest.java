650e7921f1e9dd4eb17138d396bb1f0ae59cf945
Lucia Arrabali <carmenarrabali@gmail.com>
2021-03-16 17:17:38 +0100

    Definidos los métodos de prueba

--- src/test/java/org/pab2020/FibonacciTest.java
+++ src/test/java/org/pab2020/FibonacciTest.java
@@ -1,4 +1,45 @@
 package org.pab2020;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class FibonacciTest {
+    private Fibonacci fibonacci;
+
+    @BeforeEach
+    public void setup(){
+        fibonacci = new Fibonacci();
+    }
+
+    @Test
+    public void shouldFibonacciOf1Return1() {
+        int result = fibonacci.fibonacci(1, 1);
+        int expectedValue = 1;
+
+        assertEquals(expectedValue, result);
+    }
+
+    @Test
+    public void shouldFibonacciOf2and1Return1() {
+        int result = fibonacci.fibonacci(2, 1);
+        int expectedValue = 1;
+
+        assertEquals(expectedValue, result);
+    }
+
+    @Test
+    public void shouldFibonacciOf5and3Return19() {
+        int result = fibonacci.fibonacci(5, 3);
+        int expectedValue = 19;
+
+        assertEquals(expectedValue, result);
+    }
+
+    @Test
+    public void shouldFibonacciOfMinusOneReturn____() {
+        assertThrows(RuntimeException.class, () -> fibonacci.fibonacci(-1,-1);
+    }
 }
