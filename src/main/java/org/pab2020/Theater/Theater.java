c022ac082e5180f0671c7ef20a450c181e3993c9
Lucia Arrabali <carmenarrabali@gmail.com>
2021-04-09 11:29:15 +0200

    Theater + TicketOffice

--- src/main/java/org/pab2020/Theater/Theater.java
+++ src/main/java/org/pab2020/Theater/Theater.java
@@ -9,10 +9,22 @@ public class Theater {
         int numberOfSeats = 20;
         List <Boolean> freeSeats = new ArrayList<>(numberOfSeats);
 
+        //Initialize the seats
+
         //for (int i = 0; i < numberOfSeats; i++){
             //freeSeats.add(i, true);
         //}
 
         IntStream.range(0, numberOfSeats).forEach(seat -> freeSeats.add(seat, true));
+
+        //Create the ticket offices
+        int numberOfTicketOffices = 4;
+        List<TicketOffice> ticketOffices = new ArrayList<>(numberOfTicketOffices);
+
+        for (int i = 0; i < numberOfTicketOffices; i++){
+            ticketOffices.add(new TicketOffice(i, freeSeats, numberOfSeats/numberOfTicketOffices));
+        }
+
+        System.out.println("Created " + ticketOffices.size());
     }
 }
--- src/main/java/org/pab2020/Theater/TicketOffice.java
+++ src/main/java/org/pab2020/Theater/TicketOffice.java
@@ -1,9 +1,21 @@
 package org.pab2020.Theater;
 
+import java.util.List;
+
 public class TicketOffice extends Thread{
+
+    private List<Boolean> freeSeats;
+    private int identifier;
+    private int numberOfSeats;
+
+    public TicketOffice(int identifier, List<Boolean> freeSeats, int numberOfSeats){
+        this.identifier = identifier;
+        this.freeSeats = freeSeats;
+        this.numberOfSeats = numberOfSeats;
+    }
 
     @Override
     public void run(){
-
+        System.out.println("Starting ticket office " + identifier);
     }
 }
